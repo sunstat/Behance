aff57e06c888e3791ea1bbde39f15923f3003765
Yiming Sun <sunstat@stanford.edu>
2017-06-27 14:01:09 -0700

    a

--- UserProjectNetwork/NetworkUtilities.py
+++ UserProjectNetwork/NetworkUtilities.py
@@ -14,23 +14,6 @@ from dateUtilities import DateUtilities
 from pyspark.sql.functions import udf
 
 
-def date_filer_help(date1, date2):
-    date1_arr = date1.split("-")
-    date2_arr = date2.split("-")
-    for i in range(len(date1_arr)):
-        if int(date1_arr[i]) < int(date2_arr[i]):
-            return True
-        elif int(date1_arr[i]) > int(date2_arr[i]):
-            return False
-    return True
-
-
-def date_filter(prev_date, date, end_date):
-    return date_filer_help(prev_date, date) and date_filer_help(date, end_date)
-
-
-
-
 
 
 
@@ -126,6 +109,19 @@ class NetworkUtilities(object):
 
 
     def extract_neighbors_from_users_network(self, sc):
+
+        def date_filer_help(date1, date2):
+            date1_arr = date1.split("-")
+            date2_arr = date2.split("-")
+            for i in range(len(date1_arr)):
+                if int(date1_arr[i]) < int(date2_arr[i]):
+                    return True
+                elif int(date1_arr[i]) > int(date2_arr[i]):
+                    return False
+            return True
+
+        def date_filter(prev_date, date, end_date):
+            return date_filer_help(prev_date, date) and date_filer_help(date, end_date)
 
 
         end_date = self.arguments_dict['end_day']

9bcdcb15c290ab7f99e2dd68626a86b75a5293c2
Yiming Sun <sunstat@stanford.edu>
2017-06-28 11:29:23 -0700

    a

---
+++ Test/test1.py
@@ -0,0 +1,41 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+import pyspark.sql.functions as F
+from pyspark.sql.types import StructField, StructType, StringType, LongType, DoubleType, IntegerType, BooleanType
+import os, sys
+import operator
+from scipy.sparse import coo_matrix, csr_matrix
+from subprocess import Popen
+
+
+action_file = "/Users/yimsun/PycharmProjects/Data/TinyData/action/actionDataTrimNoView-csv"
+owners_file = "/Users/yimsun/PycharmProjects/Data/TinyData/owners-csv"
+intermediate_result_dir = '../IntermediateDir'
+
+
+
+def init_spark(name, max_excutors):
+    conf = (SparkConf().setAppName(name)
+            .set("spark.dynamicAllocation.enabled", "false")
+            .set("spark.dynamicAllocation.maxExecutors", str(max_excutors))
+            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer"))
+
+    sc = SparkContext.getOrCreate(conf)
+    sc.setLogLevel('ERROR')
+    sqlContext = HiveContext(sc)
+    return sc, sqlContext
+
+sc, sqlContext = init_spark('olivia', 20)
+
+rdd_pids = sc.textFile(action_file).map(lambda x: x.split(',')).map(lambda x: (x[3], x[4])).cache()
+rdd_num_comments = rdd_pids.filter(lambda x: x[1] == 'C').groupByKey().mapValues(len)
+rdd_num_appreciations = rdd_pids.filter(lambda x: x[1] == 'A').groupByKey().mapValues(len)
+print(rdd_num_appreciations.count())
+print(rdd_num_comments.count())
+
+sqlContext.createDataFrame(rdd_pids, )
+
+pid_map_popularity = dict()
+
+
+

737c6038d0f96bace48d389b3d70b1d30f81a5e5
Yiming Sun <sunstat@stanford.edu>
2017-06-28 11:51:11 -0700

    a

--- Test/test1.py
+++ Test/test1.py
@@ -7,10 +7,17 @@ import operator
 from scipy.sparse import coo_matrix, csr_matrix
 from subprocess import Popen
 
+local = False
 
-action_file = "/Users/yimsun/PycharmProjects/Data/TinyData/action/actionDataTrimNoView-csv"
-owners_file = "/Users/yimsun/PycharmProjects/Data/TinyData/owners-csv"
-intermediate_result_dir = '../IntermediateDir'
+
+if local:
+    action_file = "/Users/yimsun/PycharmProjects/Data/TinyData/action/actionDataTrimNoView-csv"
+    owners_file = "/Users/yimsun/PycharmProjects/Data/TinyData/owners-csv"
+    intermediate_result_dir = '../IntermediateDir'
+else:
+    behance_data_dir = "wasb://testing@adobedatascience.blob.core.windows.net/behance/data"
+    action_file = os.path.join(behance_data_dir, "action", "actionDataTrimNoView-csv")
+    owners_file = os.path.join(behance_data_dir, "owners-csv")
 
 
 
@@ -33,9 +40,11 @@ rdd_num_appreciations = rdd_pids.filter(lambda x: x[1] == 'A').groupByKey().mapV
 print(rdd_num_appreciations.count())
 print(rdd_num_comments.count())
 
-sqlContext.createDataFrame(rdd_pids, )
+pidCommentsDF = sqlContext.createDataFrame(rdd_num_comments, ['pid', 'num_comments'])
+pidAppreciationsDF = sqlContext.createDataFrame(rdd_num_comments, ['pid', 'num_appreciations'])
 
+
+pidCommentsDF.show()
 pid_map_popularity = dict()
-
 
 

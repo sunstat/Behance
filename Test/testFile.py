8b3375d41682a90df2560a9744af5c6478634d6e
Yiming Sun <sunstat@stanford.edu>
2017-06-29 15:55:57 -0700

    good luck

--- Test/testFile.py
+++ Test/testFile.py
@@ -1,3 +1,5 @@
 import os
 if os.path.isfile('wasb://testing@adobedatascience.blob.core.windows.net/behance/IntermediateResult/2016-06-30/pid_2_popularity-csv'):
     print("hello world")
+else:
+    print("Hello Dessert")

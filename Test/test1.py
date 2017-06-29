efa471f8e6963e644d98433122da9b23e48fb980
Yiming Sun <sunstat@stanford.edu>
2017-06-29 11:35:20 -0700

    a

--- Test/test1.py
+++ Test/test1.py
@@ -10,6 +10,7 @@ import operator
 from scipy.sparse import coo_matrix, csr_matrix
 from IOutilities import IOutilities
 from subprocess import Popen
+from NetworkHelpFunctions import NetworkHelpFunctions
 
 
 local_run = False
@@ -69,7 +70,7 @@ class NetworkUtilities(object):
         print follow_map to intermediate directory 
         '''
         rdd = sc.textFile(action_file).map(lambda x: x.split(','))\
-            .filter(lambda x: NetworkUtilities.date_filter("0000-00-00", x[0], end_date))\
+            .filter(lambda x: NetworkHelpFunctions.date_filter("0000-00-00", x[0], end_date))\
             .filter(lambda x: x[4] == 'F').cache()
         rdd_follow = rdd.map(lambda x: (x[1], [x[2]])).reduceByKey(lambda x, y: x + y).cache()
         print(rdd_follow.take(5))

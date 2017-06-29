7bda4b40a10606805bedf11db9cceb64a5ad75eb
Yiming Sun <sunstat@stanford.edu>
2017-06-29 12:55:05 -0700

    a

--- UserProjectNetwork/NetworkUtilities.py
+++ UserProjectNetwork/NetworkUtilities.py
@@ -226,7 +226,7 @@ class NetworkUtilities(object):
         print(rdd_popularity.take(10))
         print("==================")
         #rdd_popularity = rdd_popularity.union(rdd_popularity_base)
-        rdd_popularity = rdd_popularity.map(lambda x: (x[0], calculate_popularity(x[1][0], x[1][1],1,2)))
+        rdd_popularity = rdd_popularity.map(lambda x: (x[0],x[1][0]))
         #rdd_popularity.reduceByKey(lambda x, y: x+y)
         print("======================")
         print(rdd_popularity.take(5))
@@ -234,39 +234,6 @@ class NetworkUtilities(object):
         output_file = os.path.join(output_dir, 'pid_2_popularity-csv')
         IOutilities.print_rdd_to_file(rdd_popularity, output_file, 'csv')
 
-    def calculate_increase_popularity(self, sc, intermediate_dir, base_date, cur_date):
-        def date_filer_help(date1, date2):
-            date1_arr = date1.split("-")
-            date2_arr = date2.split("-")
-            for i in range(len(date1_arr)):
-                if int(date1_arr[i]) < int(date2_arr[i]):
-                    return True
-                elif int(date1_arr[i]) > int(date2_arr[i]):
-                    return False
-            return True
-
-        def date_filter(prev_date, date, end_date_filter):
-            return date_filer_help(prev_date, date) and date_filer_help(date, end_date_filter)
-
-        def calculate_popularity(num_comments, num_appreciations, comment_weight, appreciation_weight):
-            if not num_comments:
-                return appreciation_weight*num_appreciations
-            elif not num_appreciations:
-                return comment_weight*num_comments
-            else:
-                return appreciation_weight*num_appreciations+comment_weight*num_comments
-
-        popularity_base_file = os.path.join(intermediate_dir, base_date, 'pid_2_popularity-csv')
-        rdd_popularity_base = sc.textFile(popularity_base_file).map(lambda x: x.split(','))
-        pid_set = set(rdd_popularity_base.map(lambda x: x[0]).collect())
-        pid_set_broad = sc.broadcast(pid_set)
-
-        def pid_filter(pid):
-            return pid in pid_set_broad.value
-
-        rdd_cur = sc.textFile(self.action_file).map(lambda x: x.split(',')).filter(
-            lambda x: date_filter("0000-00-00", x[0], cur_date)) \
-            .filter(lambda x: pid_filter(x[3])).map(lambda x: (x[3], x[4])).cache()
 
     def write_to_intermediate_directory(self, sc):
         end_date = self.arguments_arr[0]

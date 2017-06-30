f06702660df454cc4fba64dd4038493c9c277a23
Yiming Sun <sunstat@stanford.edu>
2017-06-30 15:06:07 -0700

    good luck

--- UserProjectNetwork/NetworkUtilities.py
+++ UserProjectNetwork/NetworkUtilities.py
@@ -74,24 +74,43 @@ class NetworkUtilities(object):
         rdd = sc.textFile(action_file).map(lambda x: x.split(','))\
             .filter(lambda x: NetworkHelpFunctions.date_filter("0000-00-00", x[0], end_date))\
             .filter(lambda x: x[4] == 'F').cache()
-        rdd_follow = rdd.map(lambda x: (x[1], [x[2]])).reduceByKey(lambda x, y: x + y).cache()
-        IOutilities.print_rdd_to_file(rdd_follow, output_file, 'psv')
-
         '''
         test num of incoming nodes
         '''
-        rdd_incoming = rdd_follow.flatMap(lambda x: x[1]).distinct()
-        print("=====================")
+
+        rdd_incoming = rdd.map(lambda x: x[2]).distinct()
+
+        print("number of nodes witn incoming degree greater than zero")
         print (rdd_incoming.take(10))
         print (rdd_incoming.count())
-        print("=====================")
+        print("number of nodes witn incoming degree greater than zero")
+        ls = rdd_incoming.collect()
+        uid_set = set(ls)
+        uid_set_broad = sc.broadcast(uid_set)
+
+
+
+
+
+        def incoming_filter(uid):
+            return uid in uid_set_broad
+
+        rdd_incoming = rdd.filter(lambda x: incoming_filter(x[1])).filter(lambda x: incoming_filter(x[2]))
+
+        rdd_follow = rdd.map(lambda x: (x[1], [x[2]])).reduceByKey(lambda x, y: x + y).cache()
+        print("pruning both out and in nodes")
+        print (rdd_incoming.take(10))
+        print (rdd_incoming.count())
+        print("pruning both out and in nodes")
+
+        IOutilities.print_rdd_to_file(rdd_follow, output_file, 'psv')
 
         '''
         print uid_index to intermediate directory
         '''
         output_file = os.path.join(output_dir, 'uid_2_index-csv')
 
-        rdd_uid_index = rdd.flatMap(lambda x: (x[1],x[2])).distinct()
+        rdd_uid_index = rdd.flatMap(lambda x: (x[1],x[2])).incoming_filter(incoming_filter).distinct()
         print("=====================")
         print (rdd_uid_index.take(10))
         print (rdd_uid_index.count())
@@ -100,10 +119,6 @@ class NetworkUtilities(object):
         print (rdd_uid_index.count())
         IOutilities.print_rdd_to_file(rdd_uid_index, output_file, 'csv')
 
-        ls = rdd_uid_index.map(lambda x: x[0]).collect()
-        uid_set = set(ls)
-
-        self.uid_set = uid_set
 
     def handle_uid_pid(self, sc, uid_set, end_date, output_dir):
 

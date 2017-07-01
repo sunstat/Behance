2fd510d3275f72b875198452a848ee64c70bcd87
Yiming Sun <sunstat@stanford.edu>
2017-07-01 09:40:18 -0700

    good luck

--- Model/FeatureExtraction/fieldExtract.py
+++ Model/FeatureExtraction/fieldExtract.py
@@ -1,6 +1,11 @@
+import numpy as np
+import scipy.sparse as sps
+from pyspark.mllib.linalg import Vectors
+
+
 class FieldExtract():
     def __init__(self, pid_2_index_file, fields_2_index_file):
         self.pid_2_index_file = pid_2_index_file
-        self.fields_2_index_fiel = fields_2_index_file
+        self.fields_2_index_file = fields_2_index_file
 
     def

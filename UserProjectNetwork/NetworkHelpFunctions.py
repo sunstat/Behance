7758fe7d4973c901776f9f39c2b882c99398023f
Yiming Sun <sunstat@stanford.edu>
2017-07-05 02:43:14 -0600

    good luck

--- UserProjectNetwork/NetworkHelpFunctions.py
+++ UserProjectNetwork/NetworkHelpFunctions.py
@@ -31,12 +31,12 @@ class NetworkHelpFunctions():
         outcoming_set = set(rdd_pair.map(lambda x : x[0]).collect())
         filter_set = incoming_set.intersection(outcoming_set)
         filter_set_broad = sc.broadcast(filter_set)
-        
+
         def filter_user(uid):
             return uid in filter_set_broad.value
 
         rdd_filtered = rdd_pair.filter(lambda x: filter_user(x[0])).filter(lambda x: filter_user(x[1])).cache()
-        while rdd_filtered.count() != len(incoming_set):
+        while rdd_filtered.count() != len(filter_set):
             incoming_set = set(rdd_pair.map(lambda x: x[1]).collect())
             outcoming_set = set(rdd_pair.map(lambda x: x[0]).collect())
             filter_set = incoming_set.intersection(outcoming_set)

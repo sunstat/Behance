f304748023a165d50af76f4df771c691932889ff
Yiming Sun <sunstat@stanford.edu>
2017-07-10 17:17:15 -0700

    good luck

--- Test/testSparseVector.py
+++ Test/testSparseVector.py
@@ -1,2 +1,8 @@
 from pyspark.mllib.linalg import *
+import numpy as np
 
+a = SparseVector(4, [1, 3], [3.0, 4.0])
+print a.dot(a)
+a.dot(np.array([1., 2., 3., 4.]))
+b = SparseVector(4, [2, 4], [1.0, 2.0])
+a.dot(b)

e9e4fe7fd583234e6f4598e160acdb18b788450a
Yiming Sun <sunstat@stanford.edu>
2017-07-10 22:36:06 -0700

    good luck

--- Model/FeatureExtraction/fieldExtract.py
+++ Model/FeatureExtraction/fieldExtract.py
@@ -6,10 +6,10 @@ from pyspark.ml.linalg import SparseVector, VectorUDT
 
 class FieldExtract():
     def __init__(self, pid_2_fields_index_file, fields_2_index_file):
-        self.pid_2_index_file = pid_2_index_file
+        self.pid_2_field_index_file = pid_2_fields_index_file
         self.fields_2_index_file = fields_2_index_file
 
     def build_feild_map(self, sc):
-        rdd_pid_2_index = sc.textFile(self.pid_2_index_file)
-        rdd_field_index_file = sc.textFile(self.fields_2_index_file)
-        rdd_field_index_file.
+        rdd_pid_2_field_index = sc.textFile(self.pid_2_field_index_file)
+        rdd_fields_2_index = sc.textFile(self.fields_2_index_file)
+

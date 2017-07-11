3895b838128fb1da0993b2f17e3c72d7b6bf920d
Yiming Sun <sunstat@stanford.edu>
2017-07-10 17:23:27 -0700

    good luck

--- Test/testSparseVector.py
+++ Test/testSparseVector.py
@@ -5,5 +5,4 @@ a = SparseVector(4, [1, 3], [3.0, 4.0])
 print a.dot(a)
 print a.dot(np.array([1., 2., 3., 4.]))
 b = SparseVector(4, [2, 4], [1.0, 2.0])
-print b
-print a.dot(b)
+print b.dot(np.array([1., 2., 3., 4.])

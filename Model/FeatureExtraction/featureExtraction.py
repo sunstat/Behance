3f00c967f25aa7ac5c4eaf3cdc4233bda75b7d28
Yiming Sun <sunstat@stanford.edu>
2017-07-11 18:17:03 -0700

    good luck

--- Model/FeatureExtraction/featureExtraction.py
+++ Model/FeatureExtraction/featureExtraction.py
@@ -16,13 +16,15 @@ class featureExtraction():
         self.rdd_popularity_file = rdd_popularity_file
 
 
-     def extractFeatures(self, sc):
-        rdd_pid_2_field_index = sc.textFile(self.pid_2_field_index_file)
-        rdd_field_2_index = sc.textFile(self.rdd_field_2_index)
-        rdd_ranks = sc.textFile(self.pid_ranks_file)
-        rdd_historic_popularity = sc.textFile(self.rdd_historic_popularity_file)
-        rdd_popularity = sc.textFile(self.rdd_historic_popularity_file)
-
+     def extract_features(self, sc):
+        rdd_pid_2_field_index = sc.textFile(self.pid_2_field_index_file).map(lambda x: x.split("#"))\
+            .map(lambda x: [x(0),tuple(x[1].split(','))])
+        rdd_field_2_index = sc.textFile(self.rdd_field_2_index).map(lambda x: x.split(','))
+        rdd_ranks = sc.textFile(self.pid_ranks_file).map(lambda x: x.split(','))
+        rdd_historic_popularity = sc.textFile(self.rdd_historic_popularity_file).map(lambda x: x.split(','))
+        rdd_popularity = sc.textFile(self.rdd_historic_popularity_file).map(lambda x: x.split(','))
+        rdd_label_data = self.build_data(rdd_pid_2_field_index, rdd_field_2_index, rdd_ranks, rdd_historic_popularity, rdd_popularity)
+        return rdd_label_data
 
     '''
     rdd_ranks [pid, score] already split
@@ -50,6 +52,7 @@ class featureExtraction():
         vec, score, historical_popularity, popularity
         '''
         rdd_label_data =  sparse_label_points(rdd_data[1][0], rdd_data[1][1], rdd_data[1][2], rdd_data[1][3], N)
+        return rdd_label_data
 
 
 

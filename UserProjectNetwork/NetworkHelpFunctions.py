f3d2f01a76d392954fd738b45304dbef3f1123dd
Yiming Sun <sunstat@stanford.edu>
2017-07-12 16:36:15 -0700

    good luck

--- UserProjectNetwork/NetworkHelpFunctions.py
+++ UserProjectNetwork/NetworkHelpFunctions.py
@@ -72,8 +72,8 @@ class NetworkHelpFunctions():
 
             rdd_pair = rdd_pair.filter(filter_set)
 
-            set1 = set(rdd_pair.map(lambda x: x[0]).distinct().collect())
-            set2 = set(rdd_pair.map(lambda x: x[1]).distinct().collect())
+            set1 = set(rdd_pair.map(lambda x: x[0]).collect())
+            set2 = set(rdd_pair.map(lambda x: x[1]).collect())
             uid_set1 = set1.intersection(set2)
             print("first element is {}, second element is {} and intersection is {}".format(len(set1), len(set2), len(uid_set1)))
             intersection_set_broad = sc.broadcast(uid_set1)
@@ -81,6 +81,7 @@ class NetworkHelpFunctions():
             def intersection_filter(x):
                 return (x[0] in intersection_set_broad.value) and (x[1] in intersection_set_broad.value)
             rdd_pair.filter(intersection_filter)
+            print(rdd_pair.take(5))
             cur_size = len(set(rdd_pair.flatMap(lambda x: (x[0],x[1])).collect()))
             iteration += 1
 

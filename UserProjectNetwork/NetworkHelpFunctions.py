4993c7f802a8ac6dd6c344c79419b93e875442ba
Yiming Sun <sunstat@stanford.edu>
2017-07-12 16:29:21 -0700

    good luck

--- UserProjectNetwork/NetworkHelpFunctions.py
+++ UserProjectNetwork/NetworkHelpFunctions.py
@@ -56,7 +56,7 @@ class NetworkHelpFunctions():
             return (x[0] in intersection_set_broad.value) and (x[1] in intersection_set_broad.value)
 
         rdd_pair.filter(intersection_filter)
-        cur_size = rdd_pair.flatMap(lambda x: (x[0], x[1])).distinct().count()
+        cur_size = len(set(rdd_pair.flatMap(lambda x: (x[0], x[1])).collect()))
 
         while (cur_size != prev_size or len(set1) != len(set2)) and iteration < n_iters:
             print "iteration : {}, cur size: {}, prev size: {}".format(iteration, cur_size, prev_size)
@@ -81,7 +81,7 @@ class NetworkHelpFunctions():
             def intersection_filter(x):
                 return (x[0] in intersection_set_broad.value) and (x[1] in intersection_set_broad.value)
             rdd_pair.filter(intersection_filter)
-            cur_size = rdd_pair.flatMap(lambda x: (x[0],x[1])).distinct().count()
+            cur_size = len(set(rdd_pair.flatMap(lambda x: (x[0],x[1])).collect()))
             iteration += 1
 
         return rdd_pair

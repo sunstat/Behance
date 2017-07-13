647caa1fdacab3534aa55f35523ed8311f77f57c
Yiming Sun <sunstat@stanford.edu>
2017-07-13 15:01:06 -0700

    good luck

--- PrerequisiteAnalysis/prerequiteAnalysis.py
+++ PrerequisiteAnalysis/prerequiteAnalysis.py
@@ -118,6 +118,7 @@ class prerequisiteAnalysis():
         arr = zip(*field_index_2_frequency)
         print arr
         pos = np.arange(len(arr[0])) + .5
+        pos_y = np.arange(len(arr[0])) + 1.5
         ylabel = []
         for index in arr[0]:
             ylabel.append(index_2_field[index])
@@ -125,7 +126,7 @@ class prerequisiteAnalysis():
         print ylabel
         plt.figure()
         plt.barh(pos, arr[1], align='center')
-        plt.yticks(pos, ylabel)
+        plt.yticks(pos_y , ylabel)
         plt.xlabel('Performance')
         plt.title('Fields Distribution')
         plt.savefig(os.path.join('../Graph/', 'histogram_of_fields.png'))

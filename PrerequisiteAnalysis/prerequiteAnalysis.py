132dc3275b791a253aaf7c473ed731e4957c52b2
Yiming Sun <sunstat@stanford.edu>
2017-07-12 17:40:33 -0700

    good luck

--- PrerequisiteAnalysis/prerequiteAnalysis.py
+++ PrerequisiteAnalysis/prerequiteAnalysis.py
@@ -93,7 +93,9 @@ class prerequisiteAnalysis():
 
     def plot_field(self, sc):
         rdd_pid_2_field_index = sc.textFile(self.pid_2_field_index_file)
-        field_2_frequency = rdd_pid_2_field_index.map(lambda x : x.split('#')).map(lambda x: x[1]).map(lambda x: x.split(','))
+        field_2_frequency = rdd_pid_2_field_index.map(lambda x : x.split('#')).map(lambda x: x[1]).map(lambda x: x.split(','))\
+            .collect()
+
         plt.figure()
         plt.hist(field_2_frequency)
         plt.title("The field Distribution")

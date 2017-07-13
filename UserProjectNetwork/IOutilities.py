ac2ccd5d65e3a961ee28d790da3592ab6e2b30b4
Yiming Sun <sunstat@stanford.edu>
2017-07-13 16:43:02 -0700

    good luck

--- UserProjectNetwork/IOutilities.py
+++ UserProjectNetwork/IOutilities.py
@@ -67,8 +67,8 @@ class IOutilities(object):
     def print_rdd_to_file(rdd, output_file, output_format):
         def to_string(x):
             return ",".join([str(y) for y in x])
-        #delete_shell_azure = os.path.join(IOutilities.shell_dir, 'delete.sh')
-        #Popen('./%s %s' % (delete_shell_azure, output_file,), shell=True)
+        delete_shell_azure = os.path.join(IOutilities.shell_dir, 'delete.sh')
+        Popen('./%s %s' % (delete_shell_azure, output_file,), shell=True)
         if output_format == 'csv':
             rdd.map(lambda x: to_string(x)).saveAsTextFile(output_file)
         elif output_format == 'psv':

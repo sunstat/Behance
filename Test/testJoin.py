1505da881d2f5f6d669eb2aab7c4075575f40431
Yiming Sun <sunstat@stanford.edu>
2017-07-14 13:33:44 -0700

    good luck

--- Test/testJoin.py
+++ Test/testJoin.py
@@ -9,9 +9,11 @@ from subprocess import Popen
 
 
 def __join_pair_rdds(rdd1, rdd2):
-    if isinstance(x[0], tuple):
-        return rdd1.join(rdd2).mapValues(lambda x: x[0] + (x[1],))
-    return rdd1.join(rdd2)
+    def f(x):
+        if isinstance(x[0], tuple):
+            return x[0] + (x[1],)
+        return (x[0], x[1])
+    return rdd1.join(rdd2).mapValues(f)
 
 def __join_list_rdds(ls_rdds):
     rdd = ls_rdds[0]

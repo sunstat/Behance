7260b4d990fb186caa1378b3a5d28a20ec57ab9c
Yiming Sun <sunstat@stanford.edu>
2017-07-14 18:33:23 -0700

    good luck

--- UserProjectNetwork/NetworkWriteForEachMonth.py
+++ UserProjectNetwork/NetworkWriteForEachMonth.py
@@ -109,7 +109,7 @@ class NetworkUtilities(object):
                                    NetworkHelpFunctions.change_none_to_zero(x[1][1]))))
         rdd_popularity = rdd_popularity.union(rdd_popularity_base)
         rdd_popularity = rdd_popularity.map(lambda x: (x[0], NetworkHelpFunctions.calculate_popularity(x[1][0],x[1][1],1,2)))
-        rdd_popularity = rdd_popularity.reduceByKey(lambda x,y : x+y) 
+        rdd_popularity = rdd_popularity.reduceByKey(lambda x,y : x+y)
         output_file = os.path.join(output_dir, '-'.join(['pid_2_popularity', 'csv']))
         print(output_file)
         IOutilities.print_rdd_to_file(rdd_popularity, output_file, 'csv')

e9d1691f43feb3452eacbb7f25c4237db85130fd
Yiming Sun <sunstat@stanford.edu>
2017-07-19 11:41:04 -0700

    good luck

---
+++ Test/testImageUrlFile.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# extract all user information from owners and actions:
+
+import sys
+
+sys.path.append('/home/yiming/Behance')
+sys.path.append('/home/yiming/Behance/configuration')
+sys.path.append('/home/yiming/Behance/UserProjectNetwork')
+
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+import pyspark.sql.functions as F
+from pyspark.sql.types import StructField, StructType, StringType, LongType, DoubleType, IntegerType, BooleanType
+import os, sys
+import operator
+from scipy.sparse import coo_matrix, csr_matrix
+from subprocess import Popen
+import matplotlib.pyplot as plt
+import numpy as np
+import configuration.constants as C
+
+
+
+def init_spark(name, max_excutors):
+    conf = (SparkConf().setAppName(name)
+            .set("spark.dynamicAllocation.enabled", "false")
+            .set("spark.dynamicAllocation.maxExecutors", str(max_excutors))
+            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer"))
+    sc = SparkContext(conf=conf)
+    sc.setLogLevel('ERROR')
+    sqlContext = HiveContext(sc)
+    return sc, sqlContext
+
+
+
+
+sc,_ = init_spark('human', 10)
+
+rdd_test = sc.textFile(C.OWNER_FILE).map(lambda x: x.split(',')).filter(lambda x: x[0] == "26721759")
+print rdd_test.take(5)
+
+

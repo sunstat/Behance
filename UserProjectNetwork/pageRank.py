88aaf22f705b658c65c5c24bc52073e77ee72269
Yiming Sun <sunstat@stanford.edu>
2017-07-20 10:40:24 -0700

    good luck

--- UserProjectNetwork/pageRank.py
+++ UserProjectNetwork/pageRank.py
@@ -43,14 +43,15 @@ class PageRank():
 
     def run(self, sc):
 
-        def compute_contribs(source_url, urls, rank):
+        def compute_contribs(urls, rank):
             # Calculates URL contributions to the rank of other URLs.
             num_urls = len(urls)
+            '''
             if len(urls) == 0:
                 yield (source_url, 0)
-            else:
-                for url in urls:
-                    yield (url, rank / num_urls)
+            '''
+            for url in urls:
+                yield (url, rank / num_urls)
 
         ranks = sc.textFile(C.UID_2_INDEX_FILE).map(lambda x: x.split(',')).map(lambda x: (x[0], 1.))
         links = sc.textFile(C.FOLLOW_MAP_FILE).map(lambda x: re.split('#', x)).map(lambda x: (x[0], x[1].split(',')))
@@ -60,7 +61,7 @@ class PageRank():
             # Calculates URL contributions to the rank of other URLs.
             contribs = links.join(ranks)
             #print contribs.take(5)
-            contribs = contribs.flatMap(lambda x: compute_contribs(x[0], x[1][0], x[1][1]))
+            contribs = contribs.flatMap(lambda x: compute_contribs(x[1][0], x[1][1]))
             # Re-calculates URL ranks based on neighbor contributions.
             #print contribs.take(5)
             ranks = contribs.reduceByKey(lambda x, y: x+y).mapValues(lambda x: x * 0.85 + 0.15)

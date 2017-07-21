b8c1a425a7d74429d1c5870b3dccffeb70c9df9e
Yiming Sun <sunstat@stanford.edu>
2017-07-21 14:32:54 -0700

    good luck

--- Model/imageModel.py
+++ Model/imageModel.py
@@ -1,24 +1,45 @@
+
 import sys
 
 sys.path.append('/home/yiming/Behance')
 sys.path.append('/home/yiming/Behance/configuration')
+sys.path.append('/home/yiming/Behance/UserProjectNetwork')
 
-
-import numpy as np
-import os
-from pyspark.mllib.linalg import Vectors
-
-from pyspark.mllib.linalg import SparseVector
-from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD, LinearRegressionModel
-
+#sys.path.append('/home/yiming/Behance/configuration/constants.py')
 import configuration.constants as C
+
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+import pyspark.sql.functions as F
+from pyspark.sql.types import StructField, StructType, StringType, LongType, DoubleType, IntegerType, BooleanType
+import os, sys
+import operator
+from scipy.sparse import coo_matrix, csr_matrix
+from IOutilities import IOutilities
+from subprocess import Popen
+from NetworkHelpFunctions import NetworkHelpFunctions
+from subprocess import call
+import configuration.constants as C
+
+
+def init_spark(name, max_excutors):
+    conf = (SparkConf().setAppName(name)
+            .set("spark.dynamicAllocation.enabled", "false")
+            .set("spark.dynamicAllocation.maxExecutors", str(max_excutors))
+            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer"))
+    sc = SparkContext(conf=conf)
+    sc.setLogLevel('ERROR')
+    sqlContext = HiveContext(sc)
+    return sc, sqlContext
+
 
 class imageModel(object):
     def __init__(self):
         pass
 
-    def feature_extraction(self):
+    def feature_extraction(self, sc):
+        rdd_image = sc.textFile(C.IMAGE_TRIMMED_FILE)
 
 
 

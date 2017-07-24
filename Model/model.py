b162704f707530df94aca548140050ce67d276b2
Yiming Sun <sunstat@stanford.edu>
2017-07-24 10:46:26 -0700

    good luck

--- Model/model.py
+++ Model/model.py
@@ -85,16 +85,22 @@ class Model():
         rdd_pid_2_field_index = sc.textFile(C.PID_2_FIELD_INDEX_FILE).map(lambda x: x.split('#'))\
             .filter(lambda x: x[0] in pid_set_broad.value)\
             .map(lambda x: (x[0], x[1].split(','))).mapValues(_vec_2_int)
+        print rdd_pid_2_field_index.take(5)
 
         rdd_pid_2_view_feature = sc.textFile(C.PID_2_VIEWS_FEATURE_FILE).map(lambda x : x.split('#')) \
             .filter(lambda x: x[0] in pid_set_broad.value)\
             .map(lambda x: (x[0], tuple(x[1].split(',')))).mapValues(_vec_2_float)
 
+        print rdd_pid_2_view_feature.take(5)
+
         rdd_pid_2_score = sc.textFile(C.PID_2_SCORE_FILE).map(lambda x: x.split(','))\
             .filter(lambda x: x[0] in pid_set_broad.value).mapValues(lambda x: float(x))
 
+        print rdd_pid_2_score.take(5)
+
         rdd_pid_2_popularity = sc.textFile(C.PID_2_POPULARITY_FILE).map(lambda x: x.split(','))\
             .filter(lambda x: x[0] in pid_set_broad.value).mapValues(lambda x: float(x))
+        print rdd_pid_2_popularity.take(5)
 
 
         print  "==================="

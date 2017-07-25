a0dffc1f81f921314017a7627dde5ccb618b7ec4
Yiming Sun <sunstat@stanford.edu>
2017-07-25 10:55:55 -0700

    good luck

--- TestModel/testIntermediateResult.py
+++ TestModel/testIntermediateResult.py
@@ -46,11 +46,11 @@ test popularity
 34164251,116
 '''
 
+def testPopularity():
 
-
-rdd = sc.textFile(C.ACTION_FILE).map(lambda x: x.split(','))
-print 2*rdd.filter(lambda x: x[3] == '15322919' and x[4] == 'A').count()\
-      +rdd.filter(lambda x: x[3] == '15322919' and x[4] == 'C').count()
+    rdd = sc.textFile(C.ACTION_FILE).map(lambda x: x.split(','))
+    print 2*rdd.filter(lambda x: x[3] == '15322919' and x[4] == 'A').count()\
+          +rdd.filter(lambda x: x[3] == '15322919' and x[4] == 'C').count()
 
 
 '''
@@ -72,21 +72,43 @@ test pagerank
 33952470,0.253337390282
 '''
 
-
-def separate(x):
-    for y in x[1]:
-        yield (x[0], y)
-
-rdd_popularity = sc.textFile(C.PID_2_POPULARITY_FILE).map(lambda x: x.split(','))
-rdd_score = sc.textFile(C.PID_2_SCORE_FILE).map(lambda x: x.spit(','))
-rdd_pid_2_uid = sc.textFile(C.PID_2_UID_FILE).map(lambda x: x.split(','))
-rdd_follow = sc.textFile(C.FOLLOW_MAP_FILE).map(lambda x: x.split('#'))
-rdd_pair = rdd_follow.map(lambda x: separate(x))
-print rdd_pair.take(5)
-
-
-
 # test pagerank makes sense or not
+
+def test_page_rank():
+    def separate(x):
+        for y in x[1]:
+            yield (x[0], y)
+
+    rdd_popularity = sc.textFile(C.PID_2_POPULARITY_FILE).map(lambda x: x.split(','))
+    rdd_score = sc.textFile(C.PID_2_SCORE_FILE).map(lambda x: x.spit(','))
+    pid_2_uid_dict = sc.textFile(C.PID_2_UID_FILE).map(lambda x: x.split(',')).collectAsMap()
+    rdd_follow = sc.textFile(C.FOLLOW_MAP_FILE).map(lambda x: x.split('#'))
+    rdd_pair = rdd_follow.flatMap(lambda x: separate(x))
+    print rdd_pair.take(5)
+    uid1 = pid_2_uid_dict['27019717']
+    uid2 = pid_2_uid_dict['38622569']
+    uid3 = pid_2_uid_dict['46419515']
+
+    print uid1 == uid2
+
+    i1 = rdd_pair.filter(lambda x: x[1] == uid1).count()
+    o1 = rdd_pair.filter(lambda x: x[0] == uid1).count()
+
+
+
+    i3 = rdd_pair.filter(lambda x: x[1] == uid3).count()
+    o3 = rdd_pair.filter(lambda x: x[0] == uid3).count()
+
+    print "incoming degree of {}  is {} and outcoming is {}".format(uid1, i1, o1)
+    print "incoming degree of {}  is {} and outcoming is {}".format(uid3, i3, o3)
+
+
+if __name__ == "__main__":
+    test_page_rank()
+
+
+
+
 
 
 

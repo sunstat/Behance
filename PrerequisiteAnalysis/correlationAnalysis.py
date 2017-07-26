ae1d1deebef15efc63641917cb68e419095d8451
Yiming Sun <sunstat@stanford.edu>
2017-07-26 10:25:02 -0700

    good luck

---
+++ PrerequisiteAnalysis/correlationAnalysis.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# extract all user information from owners and actions:
+
+import sys
+
+sys.path.append('/home/yiming/Behance')
+sys.path.append('/home/yiming/Behance/configuration')
+sys.path.append('/home/yiming/Behance/UserProjectNetwork')
+
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+import pyspark.sql.functions as F
+from pyspark.sql.types import StructField, StructType, StringType, LongType, DoubleType, IntegerType, BooleanType
+import os, sys
+import operator
+from scipy.sparse import coo_matrix, csr_matrix
+from subprocess import Popen
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import configuration.constants as C
+
+# import spark
+import NetworkHelpFunctions
+
+def init_spark(name, max_excutors):
+    conf = (SparkConf().setAppName(name)
+            .set("spark.dynamicAllocation.enabled", "false")
+            .set("spark.dynamicAllocation.maxExecutors", str(max_excutors))
+            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer"))
+    sc = SparkContext.getOrCreate(conf)
+    sc.setLogLevel('ERROR')
+    sqlContext = HiveContext(sc)
+    return sc, sqlContext
+
+
+sc, _ = init_spark('correlationAnalysis', 10)
+
+
+def correlation_page_rank():
+    pid_2_score = sc.textFile(C.PID_2_SCORE_FILE).map(lambda x: x.split(',')).mapValues(lambda x: float(x))
+    pid_2_popularity = sc.textFile(C.PID_2_POPULARITY_FILE).map(lambda x: x.split(',')).mapValues(lambda x: float(x))
+    data = pid_2_score.join(pid_2_popularity).map(lambda x: x[1]).collect()
+    data = zip(*data)
+    plt.figure()
+    fig, ax_arr = plt.subplots(1)
+    ax_arr.set_xscale("symlog")
+    ax_arr.plot(data[0],data[1])
+    ax_arr.set_title("correlation between page_rank Score and Popularity")
+    ax_arr.set_xlabel("page_rank_score")
+    ax_arr.set_ylabel("popularity")
+    plt.savefig(os.path.join('../Graph/', 'originalDegreeTailDistribution.png'))
+    plt.close()
--- Test/testImage.py
+++ Test/testImage.py
@@ -9,8 +9,6 @@ from PIL import Image
 import h5py
 from keras.models import Model
 
-
-
 from keras.layers.normalization import BatchNormalization
 from keras.layers import Input, Dense, Dropout
 from keras.models import load_model
@@ -26,7 +24,10 @@ COL_SIZE = 224
 ROW_SIZE = 224
 
 
-dataw = np.zeros([ROW_SIZE,COL_SIZE,3,2])
+
+
+'''
+dataw = np.zeros([2,ROW_SIZE,COL_SIZE,3])
 
 with open(data) as csvfile:
     row_num = 1
@@ -36,10 +37,24 @@ with open(data) as csvfile:
         file = cStringIO.StringIO(urllib.urlopen(url).read())
         data1 = np.asarray(Image.open(file).resize((ROW_SIZE, COL_SIZE), Image.ANTIALIAS))
         data1 = np.expand_dims(data1, axis=0)
+        print data1.shape
         Input(shape=(3, 200, 200), name='image_input')
         model = keras.applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None,input_shape=None)
-        feature = model.predict(data1)
+        feature = model.predict(dataw)
         model_extractfeatures = Model(input=model.input, output=model.get_layer('fc2').output)
-        feature = model_extractfeatures.predict(data1)
-        print feature[0,1:100]
+        feature = model_extractfeatures.predict(dataw)
+        print feature.shape
         break
+'''
+
+
+def image_2_numpy(url):
+    file = cStringIO.StringIO(urllib.urlopen(url).read())
+    numpy_array_image = np.asarray(Image.open(file).resize((ROW_SIZE, COL_SIZE), Image.ANTIALIAS))
+    return numpy_array_image
+
+rdd = sc.textFile()
+
+
+
+
